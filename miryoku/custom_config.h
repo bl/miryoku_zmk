178b9826a13daef4accfcccf337f6e1f7a04eebf
Bernard <bernard@laveaux.me>
2025-12-12 00:36:09 -0500

    deep sleep, boost bt, macos clipboard

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,8 @@
 
 #define MIRYOKU_NAV_VI
 
+#define MIRYOKU_CLIPBOARD_MAC
+
 // Must be defined for local builds
 #define MIRYOKU_KEYBOARD_CORNE
 
@@ -24,7 +26,7 @@
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
 &kp TAB     K00  K01    K02  K03  K04       K05  K06  K07  K08  K09  &kp BSLH \
-&kp LCTRL   K10  K11    K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&kp LCTRL   K10  K11    K12  K13  K14       K15  K16  K17  K18  K19  &kp SEMI \
 &kp LSHFT   K20  K21    K22  K23  K24       K25  K26  K27  K28  K29  &kp RSHFT \
                         K32  K33  K34       K35  K36  K37
 
