8bd09f39e7fe4dc1218696b048bbcf85e31b67fb
Bernard <bernard@laveaux.me>
2025-12-13 13:43:56 -0500

    Setup timeless homerow modifiers from urob

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -46,19 +46,25 @@
 
 #endif
 
-// Layer 0: Swapping Backspace & space on ColelmakDH
+// Layer 0: Swapping Backspace & space on ColelmakDH (with timeless HRMs)
+// Original Miryoku homerow (tap-preferred, 200ms):
+// U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),
+// U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
-U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+&hml LGUI A,       &hml LALT R,       &hml LCTRL S,      &hml LSHFT T,      &kp G,             &kp M,             &hmr LSHFT N,      &hmr LCTRL E,      &hmr LALT I,       &hmr LGUI O,       \
+U_LT(U_BUTTON, Z), &hml RALT X,       &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &hmr RALT DOT,     U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
-// Layer 1: Swapping Backspace & space on QWERTY base
+// Layer 1: Swapping Backspace & space on QWERTY base (with timeless HRMs)
+// Original Miryoku homerow (tap-preferred, 200ms):
+// U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),
+// U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),
 // NOTE: Unclear why, but input must be pressed immediately after double tap, otherwise input not registered
 #define MIRYOKU_LAYER_EXTRA \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+&hml LGUI A,       &hml LALT S,       &hml LCTRL D,      &hml LSHFT F,      &kp G,             &kp H,             &hmr LSHFT J,      &hmr LCTRL K,      &hmr LALT L,       &hmr LGUI SQT,     \
+U_LT(U_BUTTON, Z), &hml RALT X,       &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &hmr RALT DOT,     U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPACE), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 /// Define a double tap mapping to the Gaming layer
